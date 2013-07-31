6228e5a24884213c7a63cb19fe7c1b087b314292
Matt Burke <mburke@redventures.com>
2013-07-31 15:20:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElectricFieldHockey::Application.config.secret_token = 'ab2e5cd6509a120118b8434a74385337cf29739de158ce620357be7eee9ffde18e0f17c9a7919d09e58348d71163a496af507217d2bce465694f28b2acd24445'
