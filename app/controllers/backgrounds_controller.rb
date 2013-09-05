7b186a9733a697d6e69b2741634d49963049a2ac
Matt Burke <burkemd1+github@gmail.com>
2013-09-05 09:01:15 -0400

    further refactor UploadedBackground

--- app/controllers/backgrounds_controller.rb
+++ app/controllers/backgrounds_controller.rb
@@ -3,6 +3,6 @@ class BackgroundsController < ApplicationController
   def create
 
     upload = UploadedBackground.new(params[:image]).save
-    render :json => { :url => upload.file_uri}, :status => :created, :location => upload.file_uri
+    render :json => { :url => upload.uri}, :status => :created, :location => upload.uri
   end
 end
--- app/models/uploaded_background.rb
+++ app/models/uploaded_background.rb
@@ -1,17 +1,33 @@
 class UploadedBackground
-  attr_reader :file_uri
+
+  attr_reader :uri
+
   def initialize(uploaded_file_io)
     @uploaded_io = uploaded_file_io
   end
 
   def save
-    filename = SecureRandom.hex + File.extname(@uploaded_io.original_filename)
-    filepath = Rails.root.join('public','uploads', 'maps', filename)
-    @file_uri = '/uploads/maps/' + filename
-    File.open(filepath, 'w') do |file|
+    name = filename(@uploaded_io.original_filename)
+    path = filepath(name)
+    @uri = fileuri(name)
+
+    File.open(path, 'w') do |file|
       file.write( @uploaded_io.read )
     end
 
     self
   end
+
+  private
+    def filename(original_filename)
+      SecureRandom.hex + File.extname(original_filename)
+    end
+
+    def filepath(filename)
+     Rails.root.join('public','uploads', 'maps', filename)
+    end
+
+    def fileuri(filename)
+      "/uploads/maps/#{filename}"
+    end
 end
