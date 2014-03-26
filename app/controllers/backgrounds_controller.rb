c7fb644debbf433f03e4a1d3e2fd5a1b9eb55546
Matt Burke <burkemd1+github@gmail.com>
2014-03-26 09:07:57 -0400

    delete un-needed backgrounds controller

--- app/controllers/backgrounds_controller.rb
+++
@@ -1,8 +0,0 @@
-require 'securerandom'
-class BackgroundsController < ApplicationController
-  def create
-
-    upload = UploadedBackground.new(params[:image]).save
-    render :json => { :url => upload.uri}, :status => :created, :location => upload.uri
-  end
-end
