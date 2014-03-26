6e35e39746d6255b0f71c57234be30b25fe3aecb
Matt Burke <burkemd1+github@gmail.com>
2014-03-26 11:02:30 -0400

    level_win specs

--- spec/models/level_win_spec.rb
+++ spec/models/level_win_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe LevelWin do
+  describe 'attributes' do
+    let(:level_win) { LevelWin.new }
+    it 'has them' do
+      expect(level_win).to respond_to(:id)
+      expect(level_win).to respond_to(:level_json)
+      expect(level_win).to respond_to(:game_state)
+      expect(level_win).to respond_to(:attempts)
+      expect(level_win).to respond_to(:time)
+      expect(level_win).to respond_to(:created_at)
+      expect(level_win).to respond_to(:updated_at)
+      expect(level_win).to respond_to(:user)
+    end
+  end
   describe 'default order' do
     it 'should be sorted by attempts' do
       10.times do |i|
