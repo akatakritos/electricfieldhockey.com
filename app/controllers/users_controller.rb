661db1b97f135529695179b0db0901bfab369d4d
Matt Burke <burkemd1+github@gmail.com>
2014-03-28 13:19:04 -0400

    fix n+1 in users listing. [UGLY]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users = User.joins("left join levels on levels.creator_id = users.id").select("users.*, count(levels.id) as levels_count").group("users.id")
 
     respond_to do |format|
       format.html # index.html.erb
