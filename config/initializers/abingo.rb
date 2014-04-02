cde322b41bed886ad212e11d4d0a4a1882c81409
Matt Burke <burkemd1+github@gmail.com>
2014-04-02 14:11:40 -0400

    abingo plugin

---
+++ config/initializers/abingo.rb
@@ -0,0 +1,23 @@
+def ab_require(file)
+  require File.join(Rails.root, "lib", "abingo", file)
+end
+
+require File.join(Rails.root, "lib", "abingo", "abingo.rb")
+if Rails::VERSION::MAJOR >= 3
+  require File.join(Rails.root, 'lib/abingo/generators/abingo_migration/abingo_migration_generator.rb')
+
+  [
+   "abingo/conversion_rate.rb",
+   "abingo/alternative.rb",
+   "abingo/statistics.rb",
+   "abingo/experiment.rb",
+   "abingo/controller/dashboard.rb",
+   "abingo_sugar.rb",
+   "abingo_view_helper.rb",
+  ].each { |f| ab_require f }
+
+end
+
+ActionController::Base.send :include, AbingoSugar
+
+ActionView::Base.send :include, AbingoViewHelper
